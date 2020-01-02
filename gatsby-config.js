f07f1293147d3a42c090a7c0650a295708e6f1bc
Jamal Dahbur <dahbur.jamal@gmail.com>
2020-01-01 23:28:16 -0600

    First

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,123 +1,25 @@
-const path = require('path')
-const pkg = require(`./package.json`);
-
-
 module.exports = {
   siteMetadata: {
-    title: `Jamal Dahbur's Portfolio`,
-    description: `Jamal Dahbur's Portfolio`,
-    author: `@dahburj`,
-    siteURL: `https://jdahbur.com`,
+    title: "Gatsby Starter - Dimension V2",
+    author: "Hunter Chang",
+    description: "A Gatsby.js V2 Starter based on Dimension by HTML5 UP"
   },
   plugins: [
-    `gatsby-plugin-typescript`,
-    `gatsby-plugin-typescript-checker`,
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: 'gatsby-plugin-root-import',
-      options: {
-        src: path.join(__dirname, 'src'),
-        pages: path.join(__dirname, 'src/pages'),
-        root: path.join(__dirname, '.')
-      },
-    },
-    {
-      resolve: `gatsby-plugin-hotjar-tracking`,
-      options: {
-        includeInDevelopment: false,
-        id: 1628263,
-        sv: 6
-      }
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-root-import',
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#000000`,
-        theme_color: `#f7f0eb`,
-        display: `standalone`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-sass`,
-    `gatsby-plugin-styled-components`,
-    {
-      resolve: `gatsby-plugin-webfonts`,
-      options: {
-        fonts: {
-          google: [
-            {
-              family: `Roboto`,
-              variants: [`300`, `400`, `500`],
-            },
-          ],
-        },
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-react-svg',
-      options: {
-          rule: {
-            include: `${__dirname}/src/assets`
-          }
-      }
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            plugins: [`gatsby-remark-responsive-iframe`],
-            resolve: `@raae/gatsby-remark-oembed`,
-            options: {
-              // usePrefix defaults to false
-              // usePrefix: true is the same as ["oembed"]
-              usePrefix: ["oembed", "video"],
-              providers: {
-                // Important to exclude providers
-                // that adds js to the page.
-                // If you do not need them.
-                exclude: ["Reddit"]
-              }
-            }
-          }
-        ]
-      }
-    },
-    {
-      resolve: `gatsby-source-contentful`,
-      options: {
-        spaceId: `r2n8q1jk3a3h`,
-        // Learn about environment variables: https://gatsby.dev/env-vars
-        accessToken: 'VSLNvEuJd_PD7yXIBQ8jVIVasE_YmsCCWPOvSfYd1sE',
-      },
-    },
-    {
-      resolve: `gatsby-plugin-catch-links`,
-      options: {
-        excludePattern: /(excluded-link|external)/,
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    `gatsby-plugin-offline`,
-    {
-      // This is only needed temporarily. Themes will automatically be transpiled in later versions.
-      resolve: `gatsby-plugin-compile-es6-packages`,
-      options: {
-        modules: [pkg.name],
-      },
-    },
+    'gatsby-plugin-sass',
+    'gatsby-plugin-offline'
   ],
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,49 +1,56 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+import { StaticQuery, graphql } from 'gatsby'
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import '../assets/scss/main.scss'
 
-import Header from "./header"
-import "./layout-old.css"
+const Layout = ({ children, location }) => {
 
-const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+  let content;
+
+  if (location && location.pathname === '/') {
+    content = (
+      <div>
+        {children}
+      </div>
+    )
+  } else {
+    content = (
+      <div id="wrapper" className="page">
+        <div>
+          {children}
+        </div>
+      </div>
+    )
+  }
 
   return (
-    <>
-      <div
-        style={{
-          margin: `0 auto`,
-          minWidth: 500,
-          maxWidth: 100%'',
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>
-          {children}
-        </main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQuery {
+          site {
+            siteMetadata {
+              title
+            }
+          }
+        }
+      `}
+      render={data => (
+        <>
+          <Helmet
+            title={data.site.siteMetadata.title}
+            meta={[
+              { name: 'description', content: 'Sample' },
+              { name: 'keywords', content: 'sample, something' },
+            ]}
+          >
+            <html lang="en" />
+          </Helmet>
+          {content}
+        </>
+      )}
+    />
   )
 }
 
