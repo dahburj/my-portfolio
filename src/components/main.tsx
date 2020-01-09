98534006dea9fdb471b6c9f00c1056e92b310f36
Jamal Dahbur <dahbur.jamal@gmail.com>
2020-01-09 06:16:57 -0600

    Fixed up, made OG

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,8 +1,55 @@
 import PropTypes from 'prop-types'
 import React from 'react'
+import { Link } from "gatsby";
+import "../assets/scss/index-new.scss"
+
+
 import pic01 from '../images/pic01.jpg'
 import pic02 from '../images/pic02.jpg'
 import pic03 from '../images/pic03.jpg'
+
+
+const ButtonGrid = () => {
+  return(
+      <div className="sectionOne">
+        <header className="sectionOne__header">
+          <div className="sectionOne__content">
+            <ul className="boxgrid">
+              <Link to="/FaceMark/">
+                  <li className="boxgrid__item boxgrid__item">
+                    <div className="box box--facemark">
+                      <div /><div /><div />
+                    </div>
+                  </li>
+                </Link>
+              <Link to="/PosePage">
+                <li className="boxgrid__item">
+                    <div className="box box--posenet">
+                      <img src="https://img.icons8.com/ios/96/000000/yoga.png" style={{height: 150, width:150, bottom: 0, position: "fixed"}} />
+                    </div>
+                  </li>
+              </Link>
+              <Link to="/faceDetector/">
+                <li className="boxgrid__item boxgrid__item--wide">
+                  <div className="box box--image">
+                {/*    <p><span className="text--large">Face</span><br />Detect</p> */}
+                  </div>
+                </li>
+              </Link>
+              <Link to="/OpticalFlowPage/">
+                <li className="boxgrid__item boxgrid__item">
+                  <div className="box box--opticalflow">
+                    <div /><div /><div />
+                  </div>
+                </li>
+              </Link>
+            </ul>
+        </div>
+        </header>
+
+  </div>
+  )
+}
 
 class Main extends React.Component {
   render() {
@@ -21,92 +68,22 @@ class Main extends React.Component {
         id="main"
         style={this.props.timeout ? { display: 'flex' } : { display: 'none' }}
       >
+        
         <article
-          id="intro"
-          className={`${this.props.article === 'intro' ? 'active' : ''} ${
+          id="playground"
+          className={`${this.props.article === 'playground' ? 'active' : ''} ${
             this.props.articleTimeout ? 'timeout' : ''
           }`}
           style={{ display: 'none' }}
         >
-          <h2 className="major">Intro</h2>
-          <span className="image main">
-            <img src={pic01} alt="" />
-          </span>
-          <p>
-            Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin
-            aliquam facilisis ante interdum congue. Integer mollis, nisl amet
-            convallis, porttitor magna ullamcorper, amet egestas mauris. Ut
-            magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas.
-            By the way, check out my <a href="#work">awesome work</a>.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-            dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora
-            torquent per conubia nostra, per inceptos himenaeos. Etiam tristique
-            libero eu nibh porttitor fermentum. Nullam venenatis erat id
-            vehicula viverra. Nunc ultrices eros ut ultricies condimentum.
-            Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae
-            dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in
-            lectus. Pellentesque habitant morbi tristique senectus et netus et
-            malesuada fames ac turpis egestas. In non lorem sit amet elit
-            placerat maximus. Pellentesque aliquam maximus risus, vel sed
-            vehicula.
-          </p>
+          <h2 className="major">Playground</h2>
+            <div>
+              <div>
+                <ButtonGrid />
+              </div>
+            </div>
           {close}
         </article>
-
-        <article
-          id="work"
-          className={`${this.props.article === 'work' ? 'active' : ''} ${
-            this.props.articleTimeout ? 'timeout' : ''
-          }`}
-          style={{ display: 'none' }}
-        >
-          <h2 className="major">Work</h2>
-          <span className="image main">
-            <img src={pic02} alt="" />
-          </span>
-          <p>
-            Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu,
-            at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent
-            urna nisi, fringila lorem et vehicula lacinia quam. Integer
-            sollicitudin mauris nec lorem luctus ultrices.
-          </p>
-          <p>
-            Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus pharetra.
-            Pellentesque condimentum sem. In efficitur ligula tate urna.
-            Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor.
-            Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis
-            libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat
-            tempus.
-          </p>
-          {close}
-        </article>
-
-        <article
-          id="about"
-          className={`${this.props.article === 'about' ? 'active' : ''} ${
-            this.props.articleTimeout ? 'timeout' : ''
-          }`}
-          style={{ display: 'none' }}
-        >
-          <h2 className="major">About</h2>
-          <span className="image main">
-            <img src={pic03} alt="" />
-          </span>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur et adipiscing elit. Praesent
-            eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam
-            erat volutpat. Praesent urna nisi, fringila lorem et vehicula
-            lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.
-            Aliquam libero et malesuada fames ac ante ipsum primis in faucibus.
-            Cras viverra ligula sit amet ex mollis mattis lorem ipsum dolor sit
-            amet.
-          </p>
-          {close}
-        </article>
-
         <article
           id="contact"
           className={`${this.props.article === 'contact' ? 'active' : ''} ${
@@ -140,7 +117,7 @@ class Main extends React.Component {
           <ul className="icons">
             <li>
               <a
-                href="https://twitter.com/HuntaroSan"
+                href="https://twitter.com/jamaldahbur"
                 className="icon fa-twitter"
               >
                 <span className="label">Twitter</span>
